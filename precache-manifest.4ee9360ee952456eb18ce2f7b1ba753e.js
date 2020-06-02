80a1d7d793a284a44f7cdce3eef758ece0bf8874
karan mittal <mittal.karan60@gmail.com>
2020-06-03 05:12:08 +0530

    Updates

---
+++ precache-manifest.4ee9360ee952456eb18ce2f7b1ba753e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "891406d400fa7dffe5a9ac65c49ee8f9",
+    "url": "/texala/index.html"
+  },
+  {
+    "revision": "d5c30ec94d90ee5ddf0b",
+    "url": "/texala/static/css/2.bf2004ed.chunk.css"
+  },
+  {
+    "revision": "721e03a9f99e95dfe4ce",
+    "url": "/texala/static/css/main.17002d3b.chunk.css"
+  },
+  {
+    "revision": "d5c30ec94d90ee5ddf0b",
+    "url": "/texala/static/js/2.2742b03a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/texala/static/js/2.2742b03a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "721e03a9f99e95dfe4ce",
+    "url": "/texala/static/js/main.0bc6ecb0.chunk.js"
+  },
+  {
+    "revision": "955f62b645927e398f27",
+    "url": "/texala/static/js/runtime-main.d78799e2.js"
+  }
+]);
